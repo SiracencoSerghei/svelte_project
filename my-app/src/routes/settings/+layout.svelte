96e6f2d4c9040450966ccaa8224d794bf35a23b8
Siracenco Serghei <siracencoserghei@email.com>
2025-03-14 22:37:21 +0100

    some routes job

---
+++ my-app/src/routes/settings/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { LayoutProps } from '../$types.js';
+
+	let {children }: LayoutProps = $props();
+</script>
+
+<h1>Settings layout</h1>
+
+{@render children()}
---
+++ my-app/src/routes/settings/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  let message = 'Hello,Settings of SvelteKit!';
+</script>
+
+<h1>{message}</h1>
+<h2>About this site</h2>
+<a href="/settings/profile">profile</a>
+<p>TODO...</p>
+<a href="/">Home</a>

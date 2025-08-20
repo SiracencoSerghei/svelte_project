d8f5a6b0b25b9d6654de23d684da930d42724e24
Siracenco Serghei <siracencoserghei@email.com>
2025-08-20 20:59:26 +0200

    keyed each block

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,9 @@
   import Timer from './components/Timer.svelte';
   import PackageInfo from './components/PackageInfo.svelte';
   import ColorPicker from './components/ColorPicker.svelte';
+  import Thing from './components/Thing.svelte';
+
+	
 
   let name = 'Sergio';
   let src = 'https://cdn.pixabay.com/photo/2016/08/24/14/29/earth-1617121_1280.jpg';
@@ -18,8 +21,29 @@
     description: 'blazing fast',
     website: 'https://svelte.dev'
   };
+  let things = $state([
+      { id: 1, name: 'apple' },
+      { id: 2, name: 'banana' },
+      { id: 3, name: 'carrot' },
+      { id: 4, name: 'doughnut' },
+      { id: 5, name: 'egg' }
+    ]);
 
 </script>
+
+<style>
+  button,
+  Thing {
+    margin: 10px auto;
+    /* 200px зверху і знизу, auto по боках */
+    padding: 10px 20px;
+    background-color: #007bff;
+    display: block;
+    /* щоб auto працювало */
+    width: fit-content;
+    /* кнопка буде по ширині контенту */
+}
+</style>
 
 <Header {name} />
 <ImageBox {src} alt="{name} image" />
@@ -45,3 +69,14 @@
 <PackageInfo {...pkg} />
 
 <ColorPicker />
+
+<button onclick={() => things.shift()}>
+	Remove first thing
+</button>
+
+{#each things as thing}
+	<Thing name={thing.name} />
+{/each}
+
+
+

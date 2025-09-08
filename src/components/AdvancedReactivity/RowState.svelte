51ee754c5e952d7fa92a5958e0a34aee9223b83c
Siracenco Serghei <siracencoserghei@gmail.com>
2025-09-08 21:20:24 +0200

    cell class

--- src/components/AdvancedReactivity/RowState.svelte
+++ src/components/AdvancedReactivity/RowState.svelte
@@ -2,7 +2,7 @@
 	import { scale } from '../../utils/utilsadvance.js';
 	import { poll } from '../../utils/data.js';
 
-	let data = $state.row(poll());
+	let data = $state.raw(poll());
 
 	let w = $state(1);
 	let h = $state(1);
@@ -33,24 +33,37 @@
 	});
 </script>
 
-<div class="outer">
-	<svg width={w} height={h} bind:clientWidth={w} bind:clientHeight={h}>
-		<line y1={h} y2={h} x2={w} />
+<div class="cell">
+	<div class="outer">
+		<svg width={w} height={h} bind:clientWidth={w} bind:clientHeight={h}>
+			<line y1={h} y2={h} x2={w} />
 
-		{#each ticks as tick}
-			<g class="tick" transform="translate(0,{y(tick)})">
-				<line x2={w} />
-				<text x={-5}>{tick}</text>
-			</g>
-		{/each}
+			{#each ticks as tick}
+				<g class="tick" transform="translate(0,{y(tick)})">
+					<line x2={w} />
+					<text x={-5}>{tick}</text>
+				</g>
+			{/each}
 
-		<polyline points={data.map((d, i) => [x(i), y(d)]).join(' ')} />
+			<polyline points={data.map((d, i) => [x(i), y(d)]).join(' ')} />
 
-		<text x={10} y={10} font-size={36}>$SVLT</text>
-	</svg>
+			<text x={10} y={10} font-size={36}>$SVLT</text>
+		</svg>
+	</div>
 </div>
 
 <style>
+	.cell {
+		display: inline-block;
+		width: 400px;
+		height: 300px;
+		padding: 1em;
+		background: #f9f9f9;
+		border: 1px solid #ddd;
+		border-radius: 12px;
+		box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+		box-sizing: border-box;
+	}
 	.outer {
 		width: 100%;
 		height: 100%;
